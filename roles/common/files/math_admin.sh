ff966e29b6dbddfbd343e85ff631be4c2c5ed77e
serzh <nechyporchuk@gmail.com>
2015-11-19 09:43:13 +0200

    some work on math_admin build

--- roles/common/files/math_admin.sh
+++ roles/common/files/math_admin.sh
@@ -8,4 +8,4 @@ JVM_OPTS="${JVM_OPTS} -Xmx${HEAP_SIZE}"
 JVM_OPTS="${JVM_OPTS} -Dlogback.configurationFile=${BASE_DIR}/conf/logger-conf.xml"
 JVM_OPTS="${JVM_OPTS} -Dmath_engine.config_file=${BASE_DIR}/conf/config.json"
 
-java ${JVM_OPTS} -jar ${BASE_DIR}/lib/math_engine.jar
+java ${JVM_OPTS} -jar ${BASE_DIR}/lib/math_admin.jar
