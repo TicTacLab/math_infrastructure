db59c86a3fd95cbcb8f04b89833b57eea31716de
serzh <nechyporchuk@gmail.com>
2015-11-20 13:00:14 +0200

    fix logging

--- roles/common/files/math_engine.sh
+++ roles/common/files/math_engine.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-HEAP_SIZE="1g"
+HEAP_SIZE="30g"
 
 export BASE_DIR="$(dirname $0)/.."
 
 JVM_OPTS="${JVM_OPTS} -Xmx${HEAP_SIZE}"
 JVM_OPTS="${JVM_OPTS} -Dlogback.configurationFile=${BASE_DIR}/conf/logger-conf.xml"
-JVM_OPTS="${JVM_OPTS} -Dmath_admin.config_file=${BASE_DIR}/conf/config.json"
+JVM_OPTS="${JVM_OPTS} -Dmath_engine.config_file=${BASE_DIR}/conf/config.json"
 
-java ${JVM_OPTS} -jar ${BASE_DIR}/lib/math_admin.jar
+java ${JVM_OPTS} -jar ${BASE_DIR}/lib/math_engine.jar
